854bfb6afd9e64a4cd239b8c22cbab5600e81470
LordGoblin <maxime78600@gmail.com>
2019-02-13 18:34:58 +0100

    ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,25 @@ class EventsController < ApplicationController
   def new
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+    def update
+    @event = Event.find(params[:id])
+    parasol = params["event"]
+    puts "**************************"
+    puts params
+    puts parasol
+    if @event.update(title: parasol["title"], description: parasol["description"], start_date: params["start_date"].to_datetime, duration: parasol["duration"], price: parasol["price"], location: parasol["location"])
+      puts "1111111111111111111111111111111111111111"
+      redirect_to "/events/#{params["id"]}"
+    else
+      puts "2222222222222222222222222222222222222222222"
+      render :edit
+    end
+  end
+
   def create
 	@event = Event.new(title: params["title"], description: params["description"], start_date: params["start_date"], duration: params["duration"], price: params["price"], location: params["location"])
   	@event.user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/secret'
   devise_for :users
-  resources :events
+  
   resources :users
+  resources :events do
+  	resources :participations
+	end
   root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
